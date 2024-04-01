fcf31677cc2e1ce1ab6358b3485215dd9b8365e7
Rustum <rustumarellano@gmail.com>
2024-04-01 13:10:38 +0800

    fixed the fetch request

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './styles.scss'
 async function getWeather (city) {
-  const stats = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=2571b4f6c98345b38ce75524242503&q=${city}&days=1&aqi=yes&alerts=yes`)
+  const stats = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=2571b4f6c98345b38ce75524242503&q=${city}&days=1&aqi=yes&alerts=yes`,{
+    mode: 'cors'
+  })
   const data = await stats.json()
 
   const current = data.current
