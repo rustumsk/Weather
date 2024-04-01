7166fc5e18465e9f8716413ddf75dbfed953b754
Rustum <rustumarellano@gmail.com>
2024-04-01 13:07:42 +0800

    redo

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import './styles.scss'
+async function getWeather (city) {
+  const stats = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=2571b4f6c98345b38ce75524242503&q=${city}&days=1&aqi=yes&alerts=yes`)
+  const data = await stats.json()
+
+  const current = data.current
+  const forecast = data.forecast
+  const location = data.location
+
+  return [current.condition.text, location.region, current.temp_c, current.wind_kph]
+}
+
+const searchButton = document.querySelector('.ser').addEventListener('click', async () => {
+  const input = document.querySelector('#city')
+  const arr = await getWeather(input.value)
+
+  const condition = document.querySelector('.w-text')
+  condition.textContent = arr[0]
+
+  const city = document.querySelector('.l-text')
+  city.textContent = arr[1]
+
+  const temp = document.querySelector('.te')
+  temp.textContent = `${arr[2]}°C`
+
+  const wind = document.querySelector('.w')
+  wind.textContent = `${arr[3]}km/h`
+})
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  }
+}
